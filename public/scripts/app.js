0a404a308739450b023f366e83494c4e1e23d977
ZoeElizabeth <zoe.gelech@gmail.com>
2018-10-19 21:37:13 +0000

    created ajax reguest to database and have function sort data and append to matching list

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,27 +14,55 @@ $(document).ready(function() {
   
   })
 
+  // console.log(
+  // $ajax.
 
 
+  // //Loading new tweets and rending them to page
+  // function loadTweets() {
+  //   $.ajax('/api/todo', { method: 'GET' })
+  //   .then(function (tweetJSON) {
+  //     renderTweets(tweetJSON);
+  //   });
+  // }
 
+  // function renderTweets(tweets) {
+    
+  //   for (let userData in tweets) {
+    
+  //     let tweet = tweets[userData];
+  //     let newComment = createTweetElement(tweet)
+  //   //adding tweet to top of list
+  //       $('.tweets-container').prepend(newComment)
+  //     }
+  //   }
 
-  function catagorizeListContent(listData) {
-    $.ajax('/#database', { method: 'GET' })
-    .then(function (listData) {
-      console.log(listData)
+  function getListContent(data) {
+    $.ajax('/api/todo')
+    .then(function (data) {
+      for ( let itemID in data) {
+
+      let category = data[itemID].category
+      let content = data[itemID].content
+      createListContent(category, content)
       //If catagory in data base matches catagory of list add to that list
-      createListContent(listData);
-  });
+     
+
+      }
+    })
+  };
+
+  getListContent()
 
 
 
+  function createListContent(category, content) {
+    console.log(content)
+    console.log(category)
 
-  function createListContent(content) {
+    let $textContent = $('<li>').text(content)
 
-    let $content = $('<ul>')
-    // let $textContent = $('<li>').text(.cont).class(varcatgory)
-
-    content.append($textcontent)
+    $('.'+category).append($textContent)
 
     return content
 
@@ -42,6 +70,30 @@ $(document).ready(function() {
 
 
 })
+
+
+  // function categorizeListContent(category, content) {
+  //   let $textContent;
+  //   if (category === 'foods') {
+  //     createListContent(category, content)
+  //   }
+
+  // }
+
+
+
+//   function renderListContent(renderData) {
+//     $.ajax('/api/todo')
+//     .then(function (renderData) {
+//       for ( let itemID in renderData) {
+    
+//       //If catagory in data base matches catagory of list add to that list
+//       createListContent(renderData[itemID].content);
+//       }
+//     })
+//   };
+
+// renderListContent()
 
 
 
